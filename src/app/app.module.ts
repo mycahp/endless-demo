2797f79ba3072d607a475ddae8262acf81a482ac
MyCah Pleasant <mycahp@gmail.com>
2021-09-23 00:48:44 +0000

    Initial

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { StepComponent } from './pages/home/components/step/step.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    StepComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
