f690c9dd24320448170ab38731915577dbef678b
MyCah Pleasant <mycahp@gmail.com>
2021-09-25 01:42:32 +0000

    Name updates

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/singlestone-exercise'),
+      dir: require('path').join(__dirname, './coverage/endless-demo'),
       subdir: '.',
       reporters: [
         { type: 'html' },
